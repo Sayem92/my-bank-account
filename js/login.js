56b685adbd6fbcd172bf40bd3e4be320914999e4
Sayem92 <sayemmiha123@gmail.com>
2022-08-16 17:44:08 +0600

    login part done

---
+++ js/login.js
@@ -0,0 +1,15 @@
+console.log('hui')
+document.getElementById('btn-submit').addEventListener('click',function(){
+   const emailField= document.getElementById('user-email')
+   const email = emailField.value;
+
+   const passwordField = document.getElementById('user-password');
+   const password = passwordField.value ;
+
+   if(email == 'sayem@sum.com'  && password == 'sabas'){
+    console.log('ok')
+    }
+    else{
+        alert('Tui tor account password bujcos tore ami dukte dimu na!!!')
+    }
+ });
